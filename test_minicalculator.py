d52a376cf48d40392e11b66b965f86620b7b2691
Tsotetsi <me@tsotetsithapeloo.co.za>
2021-06-08 13:55:16 +0200

    Added test for subtracting

--- test_minicalculator.py
+++ test_minicalculator.py
@@ -7,3 +7,7 @@ class TestMiniCalculator(unittest.TestCase):
     def test_add(self):
         mini_instance = MiniCalculator()
         self.assertEqual(mini_instance.add(2,2), 4, "Adding 2 to 2 should equal 4")
+
+        def test_subtract(self):
+            mini_instance = MiniCalculator()
+            self.assertEqual(mini_instance.subtract((2,2), 0, "Subtracting two from two should give you zero"))
